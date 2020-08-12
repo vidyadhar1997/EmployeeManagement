cb8214cdade72a0967abd944cf07a03dce133b58
vidyadhar1997 <vidyadharhudge@gmail.com>
2020-08-12 06:13:43 -0700

    Added:-Calculating wages of total working hour reached for month

--- EmployeeManagement.sh
+++ EmployeeManagement.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -x 
+#!/bin/bash -x
 
 echo "Welcome to employee wage computation"
 
@@ -14,41 +14,46 @@ function check_attend()
   esac
 }
 
-function hour()
-{
-  val="$( check_attend $(()) )"
-  if [ $val -eq 0 ]
-  then
-      val1=$((RANDOM%2))
-   	case $val1 in 
-	     0)
-		    hours=8
-	       echo $hours ;;
-        1)	
-		    hours=4
-		    echo $hours ;;
-      esac
-  fi
+function hour(){
+val="$( check_attend $(()) )"
+if [ $val -eq 0 ]
+then
+	 val1=$((RANDOM%2))
+	 case $val1 in 
+	   0)
+		  hours=8
+	     echo $hours ;;
+      1)	
+		  hours=4
+		  echo $hours ;;
+    esac
+fi
 }
 
 function check_Employee_wage()
 {
-   val=$( hour $(()) )
-   emp_Daily_wages=$((20*val))
-   echo $emp_Daily_wages
+    val=$( hour $(()) )
+    emp_Daily_wages=$((20*val))
+    echo $emp_Daily_wages
 }
 
 function cal_wage_for_month()
 {
-   count=0
-   total_wage=1
-   while [ $count -le 20 ]
-   do
-        res="$( check_Employee_wage $(()) )"
-        total_wage=$(( $total_wage + $res ))
-        ((count++))
-   done
-        echo "$total_wage"
-}
+    count=0
+    hour=0
+    calculatinghour=0
+    total_wage=1
+    while [[ $count -le 20 ]] && [[ $hour -le 100 ]]
+    do
+       hour=$( hour $(()) )
+       calculatinghour=$(( $calculatinghour + hour ))
+       res=$( check_Employee_wage $(()) )
+       total_wage=$(( $total_wage + res ))
+       ((count++))
+    done
+       echo "$total_wage"
+       
+ }
 
-result=$( cal_wage_for_month $(()) )
+result="$( cal_wage_for_month $(()) )"
+echo $result
