8283dccd7c2714eecb7c2d4663cda56db89bf618
vidyadhar1997 <vidyadharhudge@gmail.com>
2020-08-12 05:36:54 -0700

    Added:-Part time employee wages

--- EmployeeManagement.sh
+++ EmployeeManagement.sh
@@ -4,12 +4,15 @@ echo "Welcome to employee wage computation"
 
 function check_attend()
 {
-  attend=$((RANDOM%2))
+  attend=$((RANDOM%3))
   if [ $attend -eq 0 ]
-  then
-       echo 0
+  then 
+        echo 0
+  elif [ $attend  -eq 1 ]
+  then 
+        echo 1
   else
-       echo 1
+        echo 2
   fi
 }
 
@@ -21,13 +24,13 @@ function check_Employee_wage()
   then
          emp_Daily_wages=$((20*8))
          echo $emp_Daily_wages
+  elif [ $res -eq 1 ]
+  then
+         partime_Employee_wage=$((20*4))
+         echo $partime_Employee_wages
   else
          echo "employee is absent"
   fi
 }
 
 check_Employee_wage
-
-
-
-
